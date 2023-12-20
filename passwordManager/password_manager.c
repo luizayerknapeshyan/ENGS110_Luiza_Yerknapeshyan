9327a9dced9af23e5767644c4834f290ccb52b45
luiza <luiza@Ubuntu.myguest.virtualbox.org>
2023-12-20 22:53:05 +0400

    Existing credentials and passwords removed. App title changed. PRD added

--- passwordManager/password_manager.c
+++ passwordManager/password_manager.c
@@ -252,7 +252,7 @@ int main(int argc, char *argv[]) {
     gtk_init(&argc, &argv);
 
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Authentication and Password Manager");
+    gtk_window_set_title(GTK_WINDOW(window), "Password Manager");
     gtk_container_set_border_width(GTK_CONTAINER(window), 10);
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
